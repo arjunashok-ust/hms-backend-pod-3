551eb22c987da7f6856c9cd80e561f6a00d0876d
308228 <308228@ust.com>
2026-05-09 23:22:53 +0530

    updated models and implemented get current user

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const paymentSchema = new mongoose.Schema(
     {
+
+        paymentId: { 
+            type: String, 
+            required: true, 
+            unique: true 
+        },
+
         billId: {
             type: String
         },
@@ -27,5 +34,23 @@ const paymentSchema = new mongoose.Schema(
 
     }
 )
+
+// Pre-save hook to generate sequential ID
+paymentSchema.pre('save', async function (next) {
+    if (this.isNew) {
+        try {
+            const counter = await Counter.findOneAndUpdate(
+                { name: 'payment' },
+                { $inc: { seq: 1 } }, // Creates sequence
+                { new: true, upsert: true } // upsert is update and insert
+            );
+            this.paymentId = `PAY-${String(counter.seq).padStart(6, '0')}`; // create 6 digit sequence number
+        } catch (err) {
+            return next(err);
+        }
+    }
+});
+
+module.exports = mongoose.model("Employee", employeeSchema);
 
 module.exports = mongoose.model("Payment", paymentSchema);
