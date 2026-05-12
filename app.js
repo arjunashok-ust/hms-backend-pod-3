dd82d79dab1c2c89de776389e713d57a9f1e5c29
308228 <308228@ust.com>
2026-05-12 17:53:25 +0530

    removed unwanted comments

--- app.js
+++ app.js
@@ -3,18 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
-//const { swaggerUi, swaggerDocument } = require("../swagger/swagger");
-
-
-// const employee = require("./src/models/Employee");
-// const bill = require("./src/models/Bill");
-// const counter = require("./src/models/Counter");
-//const appointment = require("./src/models/Appointment");
-// const medicalRecord = require("./src/models/MedicalRecord");
-// const patient = require("./src/models/Patient");
-// const payment = require("./src/models/Payment");
-// const user = require("./src/models/User");
+const connectDB = require("./src/config/db");
 
 // app => server object
 const app = express();
@@ -31,30 +20,11 @@ app.use(
 app.use(morgan("dev"));
 // Read JSON data sent from frontend/Postman and make it available in req.body.
 app.use(express.json());
-
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use(
-//   "/api-docs",
-//   swaggerUi.serve,
-//   swaggerUi.setup(swaggerDocument, {
-//     customCssUrl:
-//       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.min.css",
-//     customJs: [
-//       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-bundle.js",
-//       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-standalone-preset.js",
-//     ],
-//   }),
-// );
-
-
 app.get("/", (req, res) => res.json({ message: "API running" }));
 
 const authRoutes = require("./src/routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err.message));
+connectDB();
 
-module.exports = app;
+ module.exports = app;
--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
     if (mongoose.connection.readyState >= 1) return;
 
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
 };
 
 module.exports = connectDB;
