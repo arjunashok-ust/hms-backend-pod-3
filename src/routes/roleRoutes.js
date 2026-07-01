1b4a4f6fc5cfd606eec27f564132221e8902ce51
AswinAS-308247 <Aswin.AS2@ust.com>
2026-07-01 11:46:40 +0530

    Add authentication and permission checks to role routes

--- src/routes/roleRoutes.js
+++ src/routes/roleRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("../middlewares/asyncHandler");
 const roleController = require("../controllers/roleController");
+const { authenticateToken } = require("../middlewares/authMiddleware");
+const requirePermission = require("../middlewares/permissionMiddleware");
 
-router.post("/create", asyncHandler(roleController.createRole));
-router.get("/show", asyncHandler(roleController.getAllRoles));
-router.put("/:id", asyncHandler(roleController.updateRole));
-router.delete("/:id", asyncHandler(roleController.deleteRole));
+router.post("/create",authenticateToken,requirePermission("CREATE_PERMISSIONS") ,asyncHandler(roleController.createRole));
+router.get("/show",authenticateToken,requirePermission("SHOW_PERMISSIONS"), asyncHandler(roleController.getAllRoles));
+router.put("/:id",authenticateToken,requirePermission("UPDATE_PERMISSIONS"), asyncHandler(roleController.updateRole));
+router.delete("/:id",authenticateToken,requirePermission("DELETE_PERMISSIONS"), asyncHandler(roleController.deleteRole));
 
 module.exports = router;
