541cb1edc1e033f90be750e9ffd85669356c8ba8
AswinAS-308247 <Aswin.AS2@ust.com>
2026-07-07 14:44:36 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -78,12 +78,15 @@ app.use("/api/departments", departmentRoutes);
 const errorMiddleware = require("./middlewares/errorMiddleware");
 app.use(errorMiddleware);
 
-try {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB connected");
-} catch (err) {
-  console.error("MongoDB connection error:", err);
-  process.exit(1);
-}
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
+  
 
 module.exports = app;
